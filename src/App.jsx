a5ee8239b7bc3945a56154d73dfe2ed036c0489a
eyob2m <eyob2etemariam@gmail.com>
2024-11-22 21:43:22 +0300

    home page: header structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BiCart, BiSearch, BiStar } from "react-icons/bi";
+import { MdMenu } from "react-icons/md";
+import img from "./assets/4.jpg";
+import { HiHeart } from "react-icons/hi";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <header className="flex px-4 py-2 items-center bg-white  justify-between">
+        <MdMenu size={25} />
+        <h1 className="  text-5xl ">ገ</h1>
+        <div className="flex gap-2 border-solid px-3 border rounded-3xl border-gray-600 text-black items-center">
+          <BiSearch />
+          <input
+            placeholder="search..."
+            className="py-1 activ:border-none active:outline-none focus:border-none focus:outline-none  bg-transparent placeholder:text-gray-400"
+            type="text"
+          />
+        </div>
+        <BiCart size={25} />
+      </header>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
