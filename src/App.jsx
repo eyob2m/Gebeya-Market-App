bf9f3585142b39d92f76e331f2bf33284ddf21d3
eyob2m <eyob2etemariam@gmail.com>
2024-11-23 14:13:46 +0300

    home page: structured

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import img from "./assets/4.jpg";
 import { HiHeart } from "react-icons/hi";
 import { RiHeartLine } from "react-icons/ri";
 import Slider from "react-slick";
+import { FaFacebook, FaWhatsapp } from "react-icons/fa";
+import { CgTwitter } from "react-icons/cg";
 function App() {
- 
+  let categories = ["Men", "Women", "Kids", "Perfume", "Bag"];
   let settings = {
     speed: 300,
     dots: true,
@@ -15,12 +17,19 @@ function App() {
     slidesToScroll: 1,
     variableWidth: true,
   };
+  let settings2 = {
+    speed: 300,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+
+  };
   let latestCard = (
     liked,
     image,
     stage,
     gfor,
-  
+
     rating,
     title,
     sub,
@@ -28,7 +37,7 @@ function App() {
     added
   ) => {
     return (
-      <div className="p-3">
+      <div className="p-3 ">
         <div className="border-solid w-[266px] border border-[#C1C1C1] rounded-md p-2">
           <div className="p-1  relative rounded-lg">
             <div className="relative bg-[#F2F2F2]">
@@ -52,18 +61,24 @@ function App() {
               {gfor}
             </h2>
             <div className="flex items-center text-[12px] font-poppins text-[#808080] font-[400]">
-            <BiStar />
-             <div>{rating}+ rating</div>
+              <BiStar />
+              <div>{rating}+ rating</div>
             </div>
           </div>
           <div className="mt-5 px-1">
             <h1 className="text-[16px] font-poppins font-[600]">{title}</h1>
-            <h1 className="text-[12px] font-poppins font-[500] text-[#808080]">{sub}</h1>
+            <h1 className="text-[12px] font-poppins font-[500] text-[#808080]">
+              {sub}
+            </h1>
             <div className="flex justify-between items-start">
               <h2 className="text-[16px] font-poppins font-[600] self-end">
                 {price}
               </h2>
-              <button className={` rounded-lg  px-4 py-3 ${added ? "bg-gray-400" : "bg-buttonColor"}  text-[14px] font-poppins font-[500] text-white`}>
+              <button
+                className={` rounded-lg  px-4 py-3 ${
+                  added ? "bg-gray-400" : "bg-buttonColor"
+                }  text-[14px] font-poppins font-[500] text-white`}
+              >
                 {added ? "Remove from cart" : "Add to Cart"}
               </button>
             </div>
@@ -72,8 +87,69 @@ function App() {
       </div>
     );
   };
+  let categoryCard = (image, title, desc) => {
+    return (
+      <div className="p-3 ">
+        <div className="border-solid  border border-[#C1C1C1] rounded-md p-2">
+          <div className="p-10 py-4  relative rounded-lg">
+            <div className="relative bg-[#F2F2F2]">
+              <img
+                className="rounded-lg w- bg-[#F2F2F2] object-contain"
+                src={image}
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex flex-col items-center justify-center px-1">
+            <div>
+              <h1 className="text-[16px] font-poppins font-[600] text-[#000000]">
+                {title}
+              </h1>
+            </div>
+            <div className="flex justify-between items-start">
+              <h2 className="text-[12px] text-center font-poppins font-[400] text-[#808080]">
+                {desc}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  let broCategoryCard = (image, title, desc) => {
+    return (
+      <div className="p-3">
+        <div className="   rounded-md p-2">
+          <div className="p-8 py-8 mb-4 bg-[#F2F2F2] relative rounded-lg">
+            <div className="relative bg-[#F2F2F2]">
+              <img
+
+                className="rounded-lg  bg-[#F2F2F2] object-contain"
+                src={image}
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex flex-col items-center justify-center px-1">
+            <div>
+              <h1 className="text-[16px] font-poppins font-[600] text-[#000000]">
+                {title}
+              </h1>
+            </div>
+            <div className="flex justify-between items-start">
+              <h2 className="text-[12px] text-center font-poppins font-[400] text-[#808080]">
+                {desc}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
-    <div>
+    <div className="w-screen overflow-x-hidden">
       <header className="flex px-4 py-2 items-center bg-white  justify-between">
         <MdMenu size={25} />
         <h1 className="  text-5xl ">ገ</h1>
@@ -107,7 +183,9 @@ function App() {
         </div>
       </section>
       <section>
-        <h1 className="p-5 font-poppins font-[600] text-[18px]">Latest Colections</h1>
+        <h1 className="p-5 font-poppins font-[600] text-[18px]">
+          Latest Colections
+        </h1>
 
         <Slider {...settings}>
           {latestCard(
@@ -115,7 +193,7 @@ function App() {
             img,
             "In stock",
             "Women",
-           
+
             "2.3k",
             "French KIss Bag",
             "ALDO",
@@ -127,7 +205,7 @@ function App() {
             img,
             "Sold",
             "Men",
-         
+
             "3.3k",
             "Ethiopia KIss Bag",
             "ALDO",
@@ -139,7 +217,7 @@ function App() {
             img,
             "In Shipping",
             "Women",
-          
+
             "2.3k",
             " KIss Bag",
             "ALDO",
@@ -151,7 +229,7 @@ function App() {
             img,
             "In stock",
             "Kids",
-           
+
             "2.3k",
             "French KIss Bag",
             "ALDO",
@@ -160,6 +238,88 @@ function App() {
           )}
         </Slider>
       </section>
+      <section className="w-screen my-10 ">
+        <div className="p-3 overflow-x-scroll flex gap-5">
+          {categories.map((c) => {
+            return (
+              <button
+                className=" rounded-3xl py-2 px-5  font-poppins border border-[#C1C1C1] text-[#1E1E1E] text-[16px] font-[600]"
+                key={c}
+              >
+                {c}
+              </button>
+            );
+          })}
+        </div>
+        <div className="grid  grid-cols-2">
+          {categoryCard(img, "Shoes", "This is description of the product")}
+          {categoryCard(img, "Shoes", "This is description of the product")}
+          {categoryCard(img, "Shoes", "This is description of the product")}
+          {categoryCard(img, "Shoes", "This is description of the product")}
+        </div>
+        <div className="w-full mt-4 flex justify-center">
+        <button className=" rounded-lg  px-8 py-3 bg-primaryColor  text-[14px] font-poppins font-[500] text-white">
+          See more
+        </button>
+        </div>
+      </section>
+      <section className="p-4">
+        <div className=" p-4 bg-[#D0D0D0] relative">
+
+       
+        <div className=" flex flex-col gap-3 ">
+          <h1 className="text-[18px] font-poppins font-[600] text-primaryColor">Lorem ipsum dolor sit amet consectetur.</h1>
+          <h2 className="text-[14px] font-poppins font-[400] text-primaryColor">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt aperiam minima adipisci, nihil tenetur iure voluptatibus cupiditate dolorum praesentium quaerat hic aspernatur optio aliquam facilis beatae?</h2>
+        </div>
+       <div className="w-full flex justify-center">
+       <button className="rounded-lg py-2 mt-6 bg-buttonColor text-white w-[80%]">
+   
+   Explore Now
+</button>
+       </div>
+       <div className="absolute right-8 top-10 w-4 h-4 bg-primaryColor rounded-full"></div>
+       <div className="absolute right-20 top-11 w-3 h-3 bg-primaryColor rounded-full"></div>
+       <div className="absolute right-40 top-24 w-3 opacity-35 h-3 bg-primaryColor rounded-full"></div>
+       <div className="absolute right-40 top-48 w-6 opacity-60 h-6 bg-primaryColor rounded-full"></div>
+       <div className="absolute right-20 top-48 w-3 opacity-60 h-3 bg-primaryColor rounded-full"></div>
+ 
+          </div>
+      </section>
+      <section>
+
+      </section>
+      <section>
+        <h1 className="p-5 font-poppins font-[600] text-[18px]">
+          Browser by category
+        </h1>
+
+        <Slider {...settings2}>
+         {broCategoryCard(img,'Casual Wera ','Over 20 categories in staccks')}
+         {broCategoryCard(img,'Casual Wera ','Over 20 categories in staccks')}
+        </Slider>
+      </section>
+      <section>
+        <div className="bg-primaryColor text-white">
+          <div className="flex p-7 gap-2 flex-col ">
+          <div className="flex justify-between">  <h1>About</h1> <h1>^</h1></div>
+          <div className="flex justify-between">  <h1>About</h1> <h1>^</h1></div>
+          <div className="flex justify-between">  <h1>About</h1> <h1>^</h1></div>
+          
+          </div>
+          <div className="w-full flex flex-col items-center justify-center">
+          <hr  className="bg-white opacity-65 my-3 w-[80%] " />
+           <h1 className="text-3xl"> ገና</h1>
+           <div className="flex mt-5 mb-5 justify-center gap-3 w-[60%]">
+<FaFacebook  className="text-xl"/>
+<FaWhatsapp  className="text-xl"/>
+<CgTwitter  className="text-xl"/> 
+           </div>
+          </div>
+          <div>
+          </div>
+        </div>
+      </section>
+     
     </div>
   );
 }
