9c6e6fb60a98e6bf4b21a8ee7676c421da305ea8
eyob2m <eyob2etemariam@gmail.com>
2024-11-27 13:43:36 +0300

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
 
 function App(){
-  return <Checkout />
+
+  return <BrowserRouter>
+  <Routes>
+    <Route  element={<Home />} path="/"/>
+    <Route  element={<Cart />} path="/cart"/>
+    <Route  element={<Checkout />} path="/checkout"/>
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App;
